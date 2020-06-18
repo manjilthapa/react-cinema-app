1b870e5623298cdab278e082323f271442e9af9e
manjilthapa <manjilthapa@gmail.com>
2020-06-18 22:02:20 +0200

    added the slideshow component with autoslide and arrow to slide images

---
+++ src/components/contents/main-content/MainContent.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './MainContent.scss';
+import Slideshow from '../../slideshow/Slideshow';
+
+function MainContent() {
+  const images = [
+    {
+      url: 'https://image.shutterstock.com/z/stock-photo-mountains-during-sunset-beautiful-natural-landscape-in-the-summer-time-407021107.jpg'
+    },
+    {
+      url: 'https://as1.ftcdn.net/jpg/02/12/43/28/500_F_212432820_Zf6CaVMwOXFIylDOEDqNqzURaYa7CHHc.jpg'
+    },
+    {
+      url: 'https://as1.ftcdn.net/jpg/02/27/28/12/500_F_227281209_uAgEbgnOGMxvXbu1CQEmQ031pys5XJSH.jpg'
+    },
+    {
+      url: 'https://as2.ftcdn.net/jpg/01/10/95/89/500_F_110958997_WTelZic9082KUINYhW43xP9dIp78tmQG.jpg'
+    }
+  ];
+
+  return (
+    <div className="main-content">
+      <Slideshow images={images} auto={true} showArrows={true} />
+      <div className="grid-movie-title">
+        <div className="movieType">Now playing</div>
+        <div className="paginate">paginate</div>
+      </div>
+      {/* display grid component */}
+    </div>
+  );
+}
+
+export default MainContent;
---
+++ src/components/main/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Main.scss';
+import MainContent from '../contents/main-content/MainContent';
+
+function Main() {
+  return (
+    <div className="main">
+      <MainContent />
+    </div>
+  );
+}
+
+export default Main;
