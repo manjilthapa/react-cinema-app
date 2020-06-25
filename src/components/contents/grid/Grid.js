314d43e697bad08864933155d332dab6d1768bd9
manjilthapa <manjilthapa@gmail.com>
2020-06-25 09:46:42 +0200

    implemented lazy load images component

--- src/components/contents/grid/Grid.js
+++ src/components/contents/grid/Grid.js
@@ -6,6 +6,7 @@ import Rating from '../rating/Rating';
 import { IMAGE_URL } from '../../../services/movie.service';
 
 import './Grid.scss';
+import LazyImages from '../../lazy-images/LazyImages';
 
 const Grid = ({ list }) => {
   const [movies, setMovies] = useState([]);
@@ -18,7 +19,11 @@ const Grid = ({ list }) => {
       <div className="grid">
         {movies.map((movie, i) => (
           <div key={i}>
-            <div className="grid-cell" style={{ backgroundImage: `url(${IMAGE_URL}${movie.poster_path})` }}>
+            <LazyImages 
+              className="grid-cell" 
+              src={`${IMAGE_URL}${movie.poster_path}`} 
+              alt="placeholder"
+            >
               <div className="grid-read-more">
                 <button className="grid-cell-button">Read more</button>
               </div>
@@ -30,7 +35,7 @@ const Grid = ({ list }) => {
                   <div className="grid-vote-average">{movie.vote_average}</div>
                 </div>
               </div>
-            </div>
+            </LazyImages>
           </div>
         ))}
       </div>
